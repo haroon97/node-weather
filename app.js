70f488c13fdb711a95d5e8ef58e252cd7e6cdadc
Haroon Ahmed <haroonrmit@gmail.com>
2018-09-22 04:53:55 +0400

    added error handling

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ request({
   url: `http://www.mapquestapi.com/geocoding/v1/address?key=UHVW4iK5OM0GlsJ3uaq7lCjZ9h4QXA7b&location=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
-  console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
-  console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
+  if (error) {
+    console.log('Unable to connect to Map Quest servers');
+  } else if (body === undefined) {
+    console.log('Unable to find that address');
+  } else {
+    console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
+    console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
+  }
 })
