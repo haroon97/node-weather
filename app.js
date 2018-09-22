5cfbca37971d5cdad3f0ae2b1383f85c8fbe3db7
Haroon Ahmed <haroonrmit@gmail.com>
2018-09-22 04:09:11 +0400

    connected with geolocation api with dynamic address using yargs

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+  console.log(argv);
+  const encodedAddress = encodeURIComponent(argv.address);
+
+request({
+  url: `http://www.mapquestapi.com/geocoding/v1/address?key=UHVW4iK5OM0GlsJ3uaq7lCjZ9h4QXA7b&location=${encodedAddress}`,
+  json: true
+}, (error, response, body) => {
+  console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
+  console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
+})
